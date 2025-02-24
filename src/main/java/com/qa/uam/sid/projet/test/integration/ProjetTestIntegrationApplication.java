cc9fc3f7d1252134fc1f7ae784cd1c3a280d6202
bassirou ngom <bassiroungom@esp.sn>
2025-02-24 18:16:36 +0000

    add projet springboot

---
+++ src/main/java/com/qa/uam/sid/projet/test/integration/ProjetTestIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.qa.uam.sid.projet.test.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetTestIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetTestIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qa/uam/sid/projet/test/integration/ProjetTestIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.uam.sid.projet.test.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetTestIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
